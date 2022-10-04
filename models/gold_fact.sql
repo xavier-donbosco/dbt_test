31dfb59975dd631ae06bf29df7e06cd25573989b
xavier-donbosco <103046687+xavier-donbosco@users.noreply.github.com>
2022-10-04 14:20:06 +0530

    adding var

--- models/gold_fact.sql
+++ models/gold_fact.sql
@@ -3,4 +3,4 @@
   file_format='delta'
 ) }}
 
-select * from product_sales.fact_sales where date_of_entry = '2022-12-01';
+select * from product_sales.fact_sales where date_of_entry = '{{var("date")}}';
